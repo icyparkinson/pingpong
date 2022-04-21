ee455e59c8fb7ab7a1ebc442bcb247d3f5e5ba4c
koulight <koulight@hotmail.com>
2022-04-21 16:19:13 -0700

    Basic table format

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import TableContainer from "./components/TableContainer"
+
+
+
+ReactDOM.render(
+<React.StrictMode>
+    <TableContainer />
+</React.StrictMode>,
+
+
+document.getElementById("root"))
